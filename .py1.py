08ab70bf430d58f6e450c2ab6792bb1ddafd3b9a
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-10-22 12:36:32 +0530

    Delete .py1.py

--- .py1.py
+++
@@ -1,9 +0,0 @@
-a=int(input('enter a days'))
-b=int(input('enter a hours'))
-c=int(input('enter a minutes'))
-d=int(input('enter a sec'))
-a1=a*24*60*60
-a2=b*60*60
-a3=c*60
-s=a1+a2+a3+d
-print('the total no.of sec',s)
