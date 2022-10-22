71d7200ab228d445c4bc04c74ff3dce98214f418
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-10-22 12:50:08 +0530

    Add files via upload

---
+++ holiday date - Copy.py
@@ -0,0 +1,10 @@
+n=int(input('enter a holiday date:'))
+if n=='jan1':
+    print('new year day')
+elif n=='july':
+    print('canada day')
+elif n=='dec25':
+    print('cristamas day')
+else:
+    print('not applicable')
+    
