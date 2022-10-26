4d40481797d88da0db4ee54b567a09a014c592e5
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-10-26 15:29:24 +0530

    Add files via upload

---
+++ widgets&gizmos.py8.py
@@ -0,0 +1,6 @@
+widgets=int(input('no of widgets:'))
+gizmos=int(input('no of gizmos:'))
+weight_of_widgets=widgets*75
+weight_of_gizmos=gizmos*112
+both_weight=widgets*75+gizmos*112
+print('total weight is',both_weight,'grams')
