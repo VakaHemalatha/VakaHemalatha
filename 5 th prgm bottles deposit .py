c1eb9d5237e21e989b1a3becb833fad9304450b3
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-11-01 14:57:11 +0530

    Add files via upload

---
+++ 5 th prgm bottles deposit .py
@@ -0,0 +1,6 @@
+less_diposit=0.10
+more_diposit=0.25
+less=int(input('enter no of 1 liter bottels:'))
+more=int(input('enter no of 1 litter bollels:'))
+refund=less*less_deposit+more*more_diposit
+print (refund)
