8b7af0495a71743b6b1fa9b2451ff872519ffa56
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-11-01 14:57:40 +0530

    Add files via upload

---
+++ 6 th tax and tip.py
@@ -0,0 +1,9 @@
+a=str(input('enter name of food: '))
+b=float(input('enter cost of food: '))
+tip=b*(18/100)
+tax=b*(18/100)
+bill=b+tip+tax
+print('the tip is ',tip)
+print('the tax is',tax)
+print('total bill is',bill)
+
