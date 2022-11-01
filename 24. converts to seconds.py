1ceba8edc3d8c77b29736d209febc9af983c000e
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-11-01 15:17:27 +0530

    Add files via upload

---
+++ 24. converts to seconds.py
@@ -0,0 +1,12 @@
+d=int(input('enter d value:'))
+h=int(input('enter h value:'))
+m=int(input('enter m value:'))
+s=int(input('enter s value:'))
+days=d*60*60*24
+hours=h*60*60
+mins=m*60
+secs=s
+print(' no of days is:',days,'seconds')
+print('no of hours is:',hours,'seconds')
+print('no of minutes is:',mins,'seconds')
+print('no of seconds is:',secs,'seconds')
