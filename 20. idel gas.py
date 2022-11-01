e35fcf160e303d39de7892b79876d086e46281be
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-11-01 15:15:20 +0530

    Add files via upload

---
+++ 20. idel gas.py
@@ -0,0 +1,6 @@
+p=int(input('enter the pressure:'))
+v=int(input('enter the volume:'))
+t=float(input('enter the temp'))
+r=float(input('enter the gas constant:'))
+n=(p*v)/(r*(t+273.15))
+print('the number of mole is',n)
